1e8f708cc9bd4a029ab75962cd4959cbc9016568
Satyam Ghodasara <sghodas@gmail.com>
2016-07-30 16:56:35 -0400

    Implemented DataBlock struct

---
+++ datablock.go
@@ -0,0 +1,8 @@
+package DarkSky
+
+// DataBlock contains weather data for a specific location over a period of time.
+type DataBlock struct {
+	Summary string       `json:"summary"`
+	Icon    string       `json:"icon"`
+	Data    []*DataPoint `json:"data"`
+}
