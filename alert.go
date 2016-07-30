579685151eaac2eb7f0d1c69da408c9a932e0fdb
Satyam Ghodasara <sghodas@gmail.com>
2016-07-30 17:04:26 -0400

    Implemented Alert struct

---
+++ alert.go
@@ -0,0 +1,9 @@
+package DarkSky
+
+// Alert for a severe weather warning issued for a location by a governmental authority (consult the Dark Sky API documentation for a full list).
+type Alert struct {
+	Title       string  `json:"title"`
+	Expires     float64 `json:"expires"`
+	Description string  `json:"description"`
+	URI         string  `json:"uri"`
+}
