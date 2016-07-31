93bac5576f7247cadac7fa38c8365cd0a0d04681
Satyam Ghodasara <sghodas@gmail.com>
2016-07-31 15:20:56 -0400

    Added unit test for setting APIClient language

--- apiclient_test.go
+++ apiclient_test.go
@@ -1,4 +1,4 @@
-package DarkSky_test
+package darksky_test
 
 import (
 	"os"
@@ -8,22 +8,30 @@ import (
 )
 
 func TestNewAPIClient(t *testing.T) {
-	var client = DarkSky.NewAPIClient("TEST_API_KEY")
+	var client = darksky.NewAPIClient("TEST_API_KEY")
 	if client == nil {
 		t.Error("couldn't create a new API client")
 	}
 }
 
 func TestAPIClientSetUnits(t *testing.T) {
-	var client = DarkSky.NewAPIClient("TEST_API_KEY")
-	client.Units = DarkSky.SI
-	if client.Units != DarkSky.SI {
+	var client = darksky.NewAPIClient("TEST_API_KEY")
+	client.Units = darksky.SI
+	if client.Units != darksky.SI {
 		t.Error("couldn't set units on API client")
 	}
 }
 
+func TestAPIClientSetLanguage(t *testing.T) {
+	var client = darksky.NewAPIClient("TEST_API_KEY")
+	client.Language = darksky.French
+	if client.Language != darksky.French {
+		t.Error("couldn't set language on API client")
+	}
+}
+
 func TestGetForecast(t *testing.T) {
-	var client = DarkSky.NewAPIClient(os.Getenv("FORECAST_IO_API_KEY"))
+	var client = darksky.NewAPIClient(os.Getenv("FORECAST_IO_API_KEY"))
 	_, err := client.GetForecast(0, 0)
 	if err != nil {
 		t.Error(err)
