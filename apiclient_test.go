0fae642f333d12efd7c4ec061fc7f5e409f0c745
Satyam Ghodasara <sghodas@gmail.com>
2016-07-31 15:07:51 -0400

    Added unit test for creating a new APIClient

--- apiclient_test.go
+++ apiclient_test.go
@@ -1,17 +1,23 @@
 package DarkSky_test
 
 import (
-	"fmt"
+	"os"
 	"testing"
 
 	"github.com/sxg/DarkSky"
 )
 
+func TestNewAPIClient(t *testing.T) {
+	var client = DarkSky.NewAPIClient("TEST_API_KEY")
+	if client == nil {
+		t.Error("couldn't create a new API client")
+	}
+}
+
 func TestGetForecast(t *testing.T) {
-	var client DarkSky.APIClient
-	f, err := client.GetForecast(0, 0)
+	var client = DarkSky.NewAPIClient(os.Getenv("FORECAST_IO_API_KEY"))
+	_, err := client.GetForecast(0, 0)
 	if err != nil {
 		t.Error(err)
 	}
-	fmt.Println(f.Flags)
 }
