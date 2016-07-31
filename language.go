b84142e23889795ea290c9ea1ed3cc1852018b0b
Satyam Ghodasara <sghodas@gmail.com>
2016-07-30 22:27:45 -0400

    Implemented the language enum

---
+++ language.go
@@ -0,0 +1,93 @@
+package DarkSky
+
+// Language in which data from the Dark Sky API will be returned.
+type Language string
+
+const (
+	// Arabic language.
+	Arabic Language = "ar"
+
+	// Belarusian language.
+	Belarusian = "be"
+
+	// Bosnian language.
+	Bosnian = "bs"
+
+	// Czech language.
+	Czech = "cs"
+
+	// German language.
+	German = "de"
+
+	// Greek language.
+	Greek = "el"
+
+	// English language and the default.
+	English = "en"
+
+	// Spanish language.
+	Spanish = "es"
+
+	// French language.
+	French = "fr"
+
+	// Croatian language.
+	Croatian = "hr"
+
+	// Hungarian language.
+	Hungarian = "hu"
+
+	// Indonesian language.
+	Indonesian = "id"
+
+	// Italian language.
+	Italian = "it"
+
+	// Icelandic language.
+	Icelandic = "is"
+
+	// Cornish language.
+	Cornish = "kw"
+
+	// NorwegianBokmål represents the Norwegian Bokmål language.
+	NorwegianBokmål = "nb"
+
+	// Dutch language.
+	Dutch = "nl"
+
+	// Polish language.
+	Polish = "pl"
+
+	// Portuguese language.
+	Portuguese = "pt"
+
+	// Russian language.
+	Russian = "ru"
+
+	// Slovak language.
+	Slovak = "sk"
+
+	// Serbian language.
+	Serbian = "sr"
+
+	// Swedish language.
+	Swedish = "sv"
+
+	// Tetum language.
+	Tetum = "tet"
+
+	// Turkish language.
+	Turkish = "tr"
+
+	// Ukrainian language.
+	Ukrainian = "uk"
+
+	// PigLatin represents the Pig Latin language.
+	PigLatin = "x-pig-latin"
+
+	// SimplifiedChinese represents the simplified Chinese language.
+	SimplifiedChinese = "zh"
+
+	// TraditionalChinese represents the traditional Chinese language.
+	TraditionalChinese = "zh-tw"
+)
