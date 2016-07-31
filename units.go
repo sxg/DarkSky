27d94615e5f592e0aae36b746769cf48ba91400d
Satyam Ghodasara <sghodas@gmail.com>
2016-07-30 22:16:10 -0400

    Implemented Units enum

---
+++ units.go
@@ -0,0 +1,21 @@
+package DarkSky
+
+// Units in which the Dark Sky API data will be returned.
+type Units string
+
+const (
+	// SI units.
+	SI Units = "si"
+
+	// US uses United States units and the default option.
+	US = "us"
+
+	// CA uses Canadian units.
+	CA = "ca"
+
+	// UK uses United Kingdom units.
+	UK = "uk2"
+
+	// Auto uses the appropriate units based on the location for which weather data is requested.
+	Auto = "auto"
+)
