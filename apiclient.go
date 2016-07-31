d6a1662472c4818dd26d893e32c84cc2296b6c58
Satyam Ghodasara <sghodas@gmail.com>
2016-07-31 15:07:39 -0400

    Implemented constructor for APIClient

--- apiclient.go
+++ apiclient.go
@@ -4,10 +4,14 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 )
 
 // APIClient to interact with the Dark Sky API.
 type APIClient struct {
+	apiKey   string
 	Units    Units
 	Language Language
 }
@@ -16,18 +20,35 @@ const (
 	darkSkyURL = "https://api.forecast.io/forecast"
 )
 
+// NewAPIClient creates and returns a reference to a new APIClient to interact with the Dark Sky API.
+func NewAPIClient(apiKey string) *APIClient {
+	return &APIClient{apiKey: apiKey}
+}
+
+// GetForecast gets the current forecast at a specified latitude and longitude.
 func (client *APIClient) GetForecast(lat, lon float64) (*Forecast, error) {
-	resp, err := http.Get(testURL)
+	// Create URL string from host, API key, and lat/lon
+	var latLonPath = strings.Join([]string{strconv.FormatFloat(lat, 'f', 6, 64), strconv.FormatFloat(lon, 'f', 6, 64)}, ",")
+	var urlString = strings.Join([]string{darkSkyURL, client.apiKey, latLonPath}, "/")
+	var url, err = url.Parse(urlString)
+	if err != nil {
+		return nil, err
+	}
+
+	// Make a GET request to the URL
+	resp, err := http.Get(url.String())
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
+	// Read the response
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
 
+	// Parse the body into a Forecast
 	var forecast Forecast
 	err = json.Unmarshal(body, &forecast)
 	if err != nil {
